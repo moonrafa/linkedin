6c03e5458b434425276ffab16d17dee41e94b53b
Rafaelly Rodrigues <rrafasrodrigues@gmail.com>
2022-09-08 18:17:17 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Counter } from './features/counter/Counter'
+import './App.css'
+/*const firebaseConfig = {
+  apiKey: "AIzaSyC1bJrcNQ9Dshiv0aCztXgXMMBxYJ6uJV0",
+  authDomain: "linkedin-clone-91b73.firebaseapp.com",
+  projectId: "linkedin-clone-91b73",
+  storageBucket: "linkedin-clone-91b73.appspot.com",
+  messagingSenderId: "651393479098",
+  appId: "1:651393479098:web:61ec1806740cc63a7f6d83"
+};*/
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Linkedin</h1>
+    </div>
+  )
+}
+
+export default App
